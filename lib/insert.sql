f81381e267b13cd7fd48ba7a68e3d3ef259af5a3
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-20 17:50:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,38 @@
+INSERT INTO projects (name, title, category, funding_goal, start_date, end_date) VALUES
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+('Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+('Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+('The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+('Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+('I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+('I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+INSERT INTO user (name, age)
+('Finnebar', 17),
+('Bear', 6),
+('Iguana', 4),
+('Alex', 33),
+('Amanda', 24),
+('Sophie', 24),
+('Rosey', 9),
+('Victoria', 23),
+('Franz', 100),
+('Hermione', 30),
+('Voldemort', 90),
+('Marisa', 24),
+('Swizzle', 4),
+('Sirius', 36),
+('Albus', 113),
+('Squid', 5),
+('Whale', 6), 
+('Pacha', 5),
+('Ena', 24),
+('Katie', 24);
+INSERT INTO pledge(amount, user_id, project_id) VALUES
+()
+()
+()
+()
+()

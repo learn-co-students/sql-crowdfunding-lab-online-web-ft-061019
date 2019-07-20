98594488f3b6d83a1e782c5a63142312d23bf2c5
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-20 18:24:15 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO projects (name, title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES
 ('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
 ('My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
 ('The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
@@ -9,7 +9,7 @@ INSERT INTO projects (name, title, category, funding_goal, start_date, end_date)
 ('I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
 ('I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
 ('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
-INSERT INTO user (name, age)
+INSERT INTO users(name, age)
 ('Finnebar', 17),
 ('Bear', 6),
 ('Iguana', 4),
@@ -30,7 +30,7 @@ INSERT INTO user (name, age)
 ('Pacha', 5),
 ('Ena', 24),
 ('Katie', 24);
-INSERT INTO pledge(amount, user_id, project_id) VALUES
+INSERT INTO pledges(amount, user_id, project_id) VALUES
 ()
 ()
 ()

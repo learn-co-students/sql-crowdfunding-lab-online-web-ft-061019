51bc72a96afa543bf2a9502a44faabe6f3e1253e
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-22 01:05:54 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,7 @@ INSERT INTO projects(title, category, funding_goal, start_date, end_date) VALUES
 ('I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
 ('I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
 ('Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
-INSERT INTO users(name, age)
+INSERT INTO users(name, age) VALUES
 ('Finnebar', 17),
 ('Bear', 6),
 ('Iguana', 4),
@@ -31,8 +31,33 @@ INSERT INTO users(name, age)
 ('Ena', 24),
 ('Katie', 24);
 INSERT INTO pledges(amount, user_id, project_id) VALUES
-()
-()
-()
-()
-()
+(10.00, 1, 2),
+(20.00, 1, 3),
+(40.00, 1, 4),
+(50.00, 2, 3),
+(10.00, 3, 2),
+(20.00, 4, 4),
+(40.00, 5, 10),
+(60.00, 6, 10),
+(50.00, 7, 9),
+(700.00, 8, 8),
+(1000.00, 8, 7),
+(40.00, 9, 6),
+(50.00, 9, 3),
+(50.00, 10, 4),
+(24.00, 12, 1),
+(34.00, 11, 1),
+(12.00, 13, 6),
+(19.00, 14, 5),
+(20.00, 15, 5),
+(40.00, 16, 6),
+(35.50, 17, 7),
+(40.00, 18, 8),
+(60.00, 19, 9),
+(70.00, 20, 10),
+(100.00, 20, 4),
+(40.00, 19, 1),
+(20.00, 18, 6),
+(90.00, 17, 9),
+(230.00, 16, 6),
+(450.00, 15, 5);

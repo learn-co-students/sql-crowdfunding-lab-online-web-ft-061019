a394e91efb837b97f947d35a23317214a4d7c744
George Ruiz <georuiz817@gmail.com>
2019-07-25 03:12:32 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+
+CREATE TABLE users (ID INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (ID INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
